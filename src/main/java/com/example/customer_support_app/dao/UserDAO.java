a84aff0cdfd1ccd2e4045f738b78b0928747b585
radenkosetka7 <58879645+radenkosetka7@users.noreply.github.com>
2023-08-12 22:10:14 +0200

    user login changed

--- src/main/java/com/example/customer_support_app/dao/UserDAO.java
+++ src/main/java/com/example/customer_support_app/dao/UserDAO.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class UserDAO {
 
     private static ConnectionPool connectionPool = ConnectionPool.getConnectionPool();
-    private static final String SELECT_USER_BY_USERNAME = "SELECT * FROM web_shop.user WHERE status=1 and role=1 and BINARY username=?";
+    private static final String SELECT_USER_BY_USERNAME = "SELECT * FROM web_shop.employee WHERE role=1 and BINARY username=?";
     public UserDAO() {
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +26,9 @@ public class UserDAO {
             PreparedStatement ps = DBUtil.prepareStatement(c, SELECT_USER_BY_USERNAME, false);
             ps.setString(1, username);
             rs = ps.executeQuery();
-            if (rs.next()) {
-                user = new UserBean(rs.getInt("id"), rs.getString("name"), rs.getString("surname"), rs.getString("username"), rs.getString("password"));
+            if (rs.next())
+            {
+                user = new UserBean(rs.getInt("id"), rs.getString("firstname"), rs.getString("surname"), rs.getString("username"), rs.getString("password"));
             }
             ps.close();
         } catch (SQLException e) {
